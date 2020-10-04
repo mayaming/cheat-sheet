486b0bf1fb8c61a0b6faa3402e58e681f2bdb9cc
ubuntu <ubuntu@localhost.localdomain>
2020-10-05 02:49:12 +0800

    go array

--- golang/basic/hello.go
+++ golang/basic/hello.go
@@ -6,4 +6,20 @@ import (
 
 func main() {
 	fmt.Println("Hello, world")
+
+	// 创建数组
+	var cities [4]string
+	cities[0] = "Beijing"
+	cities[1] = "Shanghai"
+	cities[2] = "Guangzhou"
+	cities[3] = "Shenzhen"
+	fmt.Println(cities[0], cities[1], cities[2], cities[3])
+	fmt.Println(cities)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
+	// 创建切片
+	var singleDigitPrimes []int = primes[0:4]
+	fmt.Println(singleDigitPrimes)
 }
