2c9db971d1225787437a2a60e5bc2e9617ba38c1
mayaming <mayaming1983@gmail.com>
2020-12-26 19:14:47 +0800

    hash set for tuple

--- rust/cols/src/main.rs
+++ rust/cols/src/main.rs
@@ -6,4 +6,11 @@ fn main() {
     names.insert("Ted".to_string());
     names.insert("James".to_string());    
     println!("{:?} contains {}? {}, contains {}? {}", names, "James", names.contains("James"), "james", names.contains("james"));
+
+    let mut pairs = HashSet::new();
+    pairs.insert((1, 2));
+    pairs.insert((2, 1));
+    pairs.insert((1, 2));
+    pairs.insert((2, 3));
+    println!("{:?} contains {:?}? {}, contains {:?}? {}", pairs, (1, 2), pairs.contains(&(1, 2)), (1, 3), pairs.contains(&(1, 3)));
 }
