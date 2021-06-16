7755622bb55c09ae111fc7cdcf660549611e69aa
mayaming <mayaming1983@gmail.com>
2021-06-16 09:52:32 +0800

    keep current work

--- rust/leetcode-cat-and-mouse/src/cat_and_mouse.rs
+++ rust/leetcode-cat-and-mouse/src/cat_and_mouse.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 // https://leetcode.com/problems/cat-and-mouse/
 
@@ -17,14 +18,26 @@ impl std::fmt::Display for State {
 }
 
 impl State {
-    fn calc_result(&self, result_map: &mut HashMap<State, i32>, graph: &Vec<Vec<i32>>) -> i32 {
-        println!("check for {} {}", self.mouse_pos, self.cat_pos);
-        let mut result_next = -1;  // 额外增加一个状态-1，表示未确定
+    fn calc_result(&self, result_map: &mut HashMap<State, i32>, graph: &Vec<Vec<i32>>, visited: &mut HashSet<&State>, depth: usize, debug: bool) -> i32 {
+        if debug {
+            println!("{}check for {} {}", "  ".repeat(depth), self.mouse_pos, self.cat_pos);
+        }
+
+        match visited.get(self) {
+            Some(_) => { return 0; }  // 曾经访问过当前节点
+            None => { visited.insert(self); }
+        }
         
         match result_map.get(self) {
-            Some(-1) => { result_map.insert(self.clone(), 0); return 0; }
             Some(n) => { return *n; }
-            None => { result_map.insert(self.clone(), -1); }  // 曾经访问过当前节点，用于发现平局的情况 
+            None => {
+                if self.mouse_pos == 0 {
+                    result_map.insert(self.clone(), 1);
+                }
+                else if self.cat_pos == self.mouse_pos {
+                    result_map.insert(self.clone(), 2);
+                }
+            }
         }
 
         if self.mouse_pos == 0 {
@@ -48,7 +61,7 @@ impl State {
                         let cat_next = *cat_next;
                         if cat_next != 0 {
                             let state_next = State{mouse_pos: mouse_next, cat_pos: cat_next};
-                            let n = result_map.get(&state_next).map(|r| *r).unwrap_or_else(|| state_next.calc_result(result_map, graph));
+                            let n = result_map.get(&state_next).map(|r| *r).unwrap_or_else(|| state_next.calc_result(result_map, graph, visited, depth+1, debug));
                             match n {
                                 2 => { mouse_result_next = 2; break; }
                                 0 => { mouse_result_next = 0; }
@@ -56,7 +69,9 @@ impl State {
                                 _ => { mouse_result_next = 0 }
                             }
 
-                            println!("    stat next = {} {}, mouse result next = {}", state_next.mouse_pos, state_next.cat_pos, mouse_result_next);
+                            if debug {
+                                println!("{}mouse next {} {}: {}", "  ".repeat(depth), state_next.mouse_pos, state_next.cat_pos, mouse_result_next);
+                            }
                         }
                     }
                 }
@@ -66,12 +81,16 @@ impl State {
                     2 if result_next == -1 => { result_next = 2; }
                     _ => {}
                 }
-                // println!("  stat next = {} {}, result next = {}", self.mouse_pos, self.cat_pos, result_next);
+                if debug {
+                    println!("{}next {} {}: {}", "  ".repeat(depth), self.mouse_pos, self.cat_pos, result_next);
+                }
             }
         }
 
         result_map.insert(self.clone(), result_next);
-        println!("state: {}, result: {}", self, result_next);
+        if debug {
+            println!("{}result {} {}: {}", "  ".repeat(depth), self.mouse_pos, self.cat_pos, result_next);
+        }
         result_next
     }
 }
@@ -80,6 +99,7 @@ impl Solution {
     pub fn cat_mouse_game(graph: Vec<Vec<i32>>) -> i32 {
         let initial_stat = State{mouse_pos: 1, cat_pos: 2};
         let mut result_map = HashMap::new();
-        initial_stat.calc_result(&mut result_map, &graph)
+        let mut visited = HashSet::new();
+        initial_stat.calc_result(&mut result_map, &graph, &mut visited, 0, true)
     }
 }
