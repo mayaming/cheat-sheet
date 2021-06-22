23c3899e4be9a2439cc87472486f333d83edacae
mayaming <mayaming1983@gmail.com>
2021-06-22 12:42:57 +0800

    keep current work

--- rust/leetcode-cat-and-mouse/src/cat_and_mouse.rs
+++ rust/leetcode-cat-and-mouse/src/cat_and_mouse.rs
@@ -5,7 +5,7 @@ use std::collections::HashSet;
 
 pub struct Solution {} 
 
-#[derive(PartialEq, Eq, Hash, Clone)]
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
 struct State {
     mouse_pos: i32,
     cat_pos: i32
@@ -13,19 +13,58 @@ struct State {
 
 impl std::fmt::Display for State {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "mouse={},cat={}", self.mouse_pos, self.cat_pos)
+        write!(f, "{} {}", self.mouse_pos, self.cat_pos)
+    }
+}
+
+struct Path {
+    visited: HashSet<State>,
+    seq: Vec<State>
+}
+
+impl Path {
+    fn new(state: &State) -> Path {
+        let mut path = Path{visited: HashSet::new(), seq: vec![]};
+        path.add(state);
+        path
+    }
+
+    fn add(&mut self, state: &State) -> bool {
+        match self.visited.get(state) {
+            Some(_) => { false }
+            None => {
+                self.visited.insert(state.clone());
+                self.seq.push(state.clone());
+                true
+            }
+        }
+    }
+
+    fn leave(&mut self) -> () {
+        let state = self.seq.last().unwrap();
+        self.visited.remove(state);
+        self.seq.truncate(self.seq.len()-1);
+    }
+
+    fn can_go_back(&self, mouse_next_pos: i32) -> bool {
+        self.seq.len() > 1 && self.seq[self.seq.len()-2].mouse_pos == mouse_next_pos
+    }
+}
+
+impl std::fmt::Display for Path {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut s = String::new();
+        for state in &self.seq {
+            s.push_str((format!("{} -> ", state)).as_str());
+        }
+        write!(f, "{}", s)
     }
 }
 
 impl State {
-    fn calc_result(&self, result_map: &mut HashMap<State, i32>, graph: &Vec<Vec<i32>>, visited: &mut HashSet<State>, depth: usize, debug: bool) -> i32 {
-        if debug {
-            println!("{}check for {} {}", "  ".repeat(depth), self.mouse_pos, self.cat_pos);
-        }
-
-        match visited.get(self) {
-            Some(_) => { return 0; }  // 曾经访问过当前节点
-            None => { visited.insert(self.clone()); }
+    fn calc_result(&self, result_map: &mut HashMap<State, i32>, graph: &Vec<Vec<i32>>, path: &mut Path, prepare: bool, depth: usize, debug: bool) -> i32 {
+        if debug && !prepare {
+            println!("{}check for {}, path = {}", "  ".repeat(depth), self, path);
         }
         
         let ret = match result_map.get(self) {
@@ -44,7 +83,7 @@ impl State {
                     
                     for mouse_next in mouse_nexts {
                         let mouse_next = *mouse_next;
-                        let mut mouse_result_next = -1;
+                        let mut mouse_result_next = if path.can_go_back(mouse_next) { 0 } else { -1 };
                         if mouse_next == self.cat_pos {
                             mouse_result_next = 2;
                         }
@@ -56,48 +95,60 @@ impl State {
                                 let cat_next = *cat_next;
                                 if cat_next != 0 {
                                     let state_next = State{mouse_pos: mouse_next, cat_pos: cat_next};
-                                    let n = result_map.get(&state_next).map(|r| *r).unwrap_or_else(|| state_next.calc_result(result_map, graph, visited, depth+1, debug));
-                                    match n {
-                                        2 => { mouse_result_next = 2; break; }
-                                        0 => { mouse_result_next = 0; }
-                                        1 if mouse_result_next == -1 => { mouse_result_next = 1; }
-                                        _ => {}
-                                    }
-        
-                                    if debug {
-                                        println!("{}mouse next {} {}: {}", "  ".repeat(depth), state_next.mouse_pos, state_next.cat_pos, mouse_result_next);
+                                    if path.add(&state_next) {
+                                        let n = result_map.get(&state_next).map(|r| *r).unwrap_or_else(|| state_next.calc_result(result_map, graph, path, prepare, depth+1, debug));
+                                        path.leave();
+                                        if debug && !prepare {
+                                            println!("{}mouse goes {} cat goes {}, got {}, mouse_result_next = {}", "  ".repeat(depth), state_next.mouse_pos, state_next.cat_pos, n, mouse_result_next);
+                                        }
+                                        match n {
+                                            2 => { mouse_result_next = 2; break; }
+                                            0 => { mouse_result_next = 0; }
+                                            1 if mouse_result_next == -1 || mouse_result_next == 1 => { mouse_result_next = 1; }
+                                            _ => {}
+                                        }
                                     }
                                 }
                             }
                         }
+
                         match mouse_result_next {
                             1 => { result_next = 1; break; }
                             0 => { result_next = 0; }
-                            2 if result_next == -1 => { result_next = 2; }
-                            _ => {}
+                            2 if result_next == -1 || result_next == 2 => { result_next = 2; }
+                            _ => { result_next = 0; }
                         }
-                        if debug {
-                            println!("{}next {} {}: {}", "  ".repeat(depth), self.mouse_pos, self.cat_pos, result_next);
-                        }
+
                     }
 
-                    result_map.insert(self.clone(), result_next);
+                    if debug && !prepare {
+                        println!("{}result for {}: {}", "  ".repeat(depth), self, result_next);
+                    }
+
+
+                    if !prepare || result_next == 1 || result_next == 2 {
+                        result_map.insert(self.clone(), result_next);
+                    }
                 }
 
-                *result_map.get(self).unwrap()
+                *result_map.get(self).unwrap_or(&0)
             }
         };
 
-        visited.remove(self);
         ret
     }
 }
 
 impl Solution {
     pub fn cat_mouse_game(graph: Vec<Vec<i32>>) -> i32 {
+        let debug = false;
         let initial_stat = State{mouse_pos: 1, cat_pos: 2};
         let mut result_map = HashMap::new();
-        let mut visited = HashSet::new();
-        initial_stat.calc_result(&mut result_map, &graph, &mut visited, 0, false)
+        let mut path = Path::new(&initial_stat);
+        initial_stat.calc_result(&mut result_map, &graph, &mut path, true, 0, debug);
+        if debug {
+            println!("result map after preparation: {:?}", result_map);
+        }
+        initial_stat.calc_result(&mut result_map, &graph, &mut path, false, 0, debug)
     }
 }
--- rust/leetcode-cat-and-mouse/src/main.rs
+++ rust/leetcode-cat-and-mouse/src/main.rs
@@ -1,12 +1,15 @@
 mod cat_and_mouse;
 
 fn main() {
-    println!("{}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![1,3], vec![0], vec![3], vec![0,2]]));
-    println!("{}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![2,5], vec![3], vec![0,4,5], vec![1,4,5], vec![2,3], vec![0,2,3]]));
+    println!("1 -> {}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![1,3], vec![0], vec![3], vec![0,2]]));
+    println!("0 -> {}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![2,5], vec![3], vec![0,4,5], vec![1,4,5], vec![2,3], vec![0,2,3]]));
     // [[2,3],[3,4],[0,4],[0,1],[1,2]]
-    println!("{}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![2,3], vec![3,4], vec![0,4], vec![0,1], vec![1,2]]));
+    println!("1 -> {}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![2,3], vec![3,4], vec![0,4], vec![0,1], vec![1,2]]));
     // [[2,3],[2],[0,1],[0,4],[3]]
-    println!("{}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![2,3], vec![2], vec![0,1], vec![0,4], vec![3]]));
+    println!("2 -> {}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![2,3], vec![2], vec![0,1], vec![0,4], vec![3]]));
     // [[6],[4,11],[9,12],[5],[1,5,11],[3,4,6],[0,5,10],[8,9,10],[7],[2,7,12],[6,7],[1,4],[2,9]]
-    println!("{}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![6], vec![4,11], vec![9,12], vec![5], vec![1,5,11], vec![3,4,6], vec![0,5,10], vec![8,9,10], vec![7], vec![2,7,12], vec![6,7], vec![1,4], vec![2,9]]));
+    println!("1 -> {}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![6], vec![4,11], vec![9,12], vec![5], vec![1,5,11], vec![3,4,6], vec![0,5,10], vec![8,9,10], vec![7], vec![2,7,12], vec![6,7], vec![1,4], vec![2,9]]));
+    // [[2,3,4],[2,4],[0,1,4],[0,4],[0,1,2,3]]
+    println!("2 -> {}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![2,3,4], vec![2,4], vec![0,1,4], vec![0,4], vec![0,1,2,3]]));
+    println!("0 -> {}", cat_and_mouse::Solution::cat_mouse_game(vec![vec![2,9,14], vec![2,5,7], vec![0,1,3,8,9,11,14], vec![2,12], vec![5,11,18], vec![1,4,18], vec![9,17,19], vec![1,11,12,13,14,17,19], vec![2,16,17], vec![0,2,6,12,14,18], vec![14], vec![2,4,7], vec![3,7,9,13], vec![7,12,14], vec![0,2,7,9,10,13,17], vec![18], vec![8,19], vec![6,7,8,14,19], vec![4,5,9,15], vec![6,7,16,17]]));
 }
